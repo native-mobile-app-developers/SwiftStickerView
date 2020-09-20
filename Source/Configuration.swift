aa7d07d35fad31a89cc2930bba8e4689be34adde
sachithamh <sachithamh@gmail.com>
2020-09-20 16:00:07 +0530

    addscale delete functions

---
+++ Source/Configuration.swift
@@ -0,0 +1,8 @@
+//
+//  Configuration.swift
+//  SwiftStickerView
+//
+//  Created by sachitha on 9/19/20.
+//
+
+import Foundation
---
+++ Source/StickerView.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftStickerView.swift
+//  SwiftStickerView
+//
+//  Created by sachitha on 9/19/20.
+//
+
+import Foundation
+public class SwiftStickerView:UIView{
+    
+}
